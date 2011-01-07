cbb785d4dbbfb377bc2479de9519d35a8d389828
crazycode <crazycode@gmail.com>
2011-01-07 13:52:07 +0800

    use command line argv[0] specify configure file

--- lib/check-taskr/cli.rb
+++ lib/check-taskr/cli.rb
@@ -2,15 +2,23 @@
 require "fastthread"
 require "check-taskr/base"
 
+
 Dir[File.join(File.dirname(__FILE__), 'task/*.rb')].sort.each { |lib| require lib }
 
 module CheckTaskr
   class Cli
 
-    def self.execute(options = {})
+    def self.execute(filename, options = {})
+      logdir = options.delete(:logdir)
+      # create a logger named 'mylog' that logs to stdout
+      log = Logger.new 'default'
+      log.outputters =  Log4r::DateFileOutputter.new('check_log', :dirname => logdir)
+      log.level = WARN
+
+      log.debug("load #{filename} file, log on #{logdir} ...")
 
       config = CheckTaskr::JobsConfiguration.instance
-      config.load_from_file("jobs")
+      config.load_from_file(filename)
 
       t = Thread.new do
         while true do
